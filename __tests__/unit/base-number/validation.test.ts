4a33499cb8d59c99a7d985856be1fa3ee45875f4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-02 12:22:14 +0200

    🧪 check validation

--- __tests__/unit/base-number/validation.test.ts
+++ __tests__/unit/base-number/validation.test.ts
@@ -1,10 +1,23 @@
 import { describe, expect, test } from 'vitest';
 
-import { decimal } from '@/utils/base-number';
+import { binary, decimal } from '@/utils/base-number';
 
 describe('Base number validation on creation', () => {
   test('Decimal value is always valid, because it is using native number type', () => {
     const value = decimal(12);
     expect(value.value).toBe(12);
+  });
+
+  test('Binary can not be created from not a bitmap string', () => {
+    expect(binary('not a binary')).toBe(null);
+  });
+
+  test('Binary creator does not accept decimal values as string', () => {
+    expect(binary('120329')).toBe(null);
+  });
+
+  test('Binary does not accept negative values', () => {
+    expect(binary('-12')).toBe(null);
+    expect(binary('-10011010')).toBe(null);
   });
 });
