bf72759b4bacd2bbaf56f4a3c7c8561d9c641e6e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-02 22:23:19 +0200

    🎚️ add sliders to ui

--- src/components/ui/NetMap/NetMap.tsx
+++ src/components/ui/NetMap/NetMap.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { Slider } from '@mui/material';
 import cn from 'classnames';
+import { Fragment } from 'react';
 
+import { VStack } from '@/components/ui';
 import { useNetworkStore } from '@/zustand';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
@@ -11,5 +14,26 @@ export function NetMap() {
   const min = root?.address.asFullDecimal();
   const max = root?.broadcast.asFullDecimal();
 
-  return <div className={cn('w-full bg-zinc-800 p-[1.6rem]')}>Net map</div>;
+  return (
+    <VStack
+      className={cn('w-full bg-zinc-800 p-[1.6rem]')}
+      spacing='0.8rem'
+    >
+      {subnets.map(({ network }, idx) => (
+        <Fragment key={idx}>
+          <Slider
+            min={min}
+            max={max}
+            value={[
+              network.address.asFullDecimal(),
+              network.broadcast.asFullDecimal(),
+            ]}
+            disabled
+            disableSwap
+            className={cn('[&>.MuiSlider-thumb]:size-[0.6rem]')}
+          />
+        </Fragment>
+      ))}
+    </VStack>
+  );
 }
