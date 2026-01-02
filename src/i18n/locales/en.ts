5fd82dd3c003b9be131b73b90713d990c430b7fe
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-02 21:27:40 +0200

    🔤 update locales

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -8,6 +8,7 @@ export const en = {
       wrongFormat:
         'Wrong format of address (the correct one is "192.168.0.1/{1-31}")',
       wrongMask: 'Mask must be an integer (1-31)',
+      subnetOutsideRoot: 'Address is outside root network.',
     },
   },
 } satisfies RecursiveKeyValuePair;
