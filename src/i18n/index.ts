dee077db944e031ea43a038db28b9d3553fde95c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-02 18:04:12 +0200

    📝 change return

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -7,5 +7,7 @@ import { appLocales } from './locales';
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const useTranslations = () => {
   const params = useParams<{ lang: string }>();
-  return appLocales[params.lang as Locales];
+  return {
+    t: appLocales[params.lang as Locales],
+  };
 };
