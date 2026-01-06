9181516235d0d16d67d4802c2dc60d3055bdede9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-06 22:42:12 +0200

    🐟 change comment

--- __tests__/unit/utilities/get-ranges-overlaps.test.tsx
+++ __tests__/unit/utilities/get-ranges-overlaps.test.tsx
@@ -4,10 +4,10 @@ import { getRangesOverlaps } from '@/utils/misc';
 
 describe('getRangesOverlaps fn', () => {
   test('It works', () => {
-    // 0 1 2 3 4 5 6 7 8 9 10
-    //           5 6 7 8 9 10 11 12 13 14 15
-    //                 8 9 10 11 12
-    //   1 2 3 4 5 6 7 8 9
+    // 0 ================= 10
+    //           5 ====================== 15
+    //                 8 ======= 12
+    //   1 ============= 9
     const ranges: [number, number][] = [
       [0, 10],
       [5, 15],
