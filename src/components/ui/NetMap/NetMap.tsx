c716664775c7830944b487f941363fc9eec4cbaa
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-06 23:12:42 +0200

    📝 refactor code

--- src/components/ui/NetMap/NetMap.tsx
+++ src/components/ui/NetMap/NetMap.tsx
@@ -2,9 +2,9 @@
 
 import { Slider } from '@mui/material';
 import cn from 'classnames';
-import { Fragment } from 'react';
 
-import { VStack } from '@/components/ui';
+import { For } from '@/components/layout';
+import { VStack, ZStack } from '@/components/ui';
 import { useNetworkStore } from '@/zustand';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
@@ -16,25 +16,28 @@ export function NetMap() {
   const max = root?.broadcast.asFullDecimal();
 
   return (
-    <VStack
-      className={cn('w-full bg-zinc-800 p-[1.6rem]')}
-      spacing='0.8rem'
-    >
-      {subnets.map(({ network }, idx) => (
-        <Fragment key={idx}>
-          <Slider
-            min={min}
-            max={max}
-            value={[
-              network.address.asFullDecimal(),
-              network.broadcast.asFullDecimal(),
-            ]}
-            disabled
-            disableSwap
-            className={cn('[&>.MuiSlider-thumb]:size-[0.6rem]')}
-          />
-        </Fragment>
-      ))}
-    </VStack>
+    <ZStack className={cn('w-full bg-zinc-800 p-[1.6rem]')}>
+      <VStack
+        className={cn('w-full')}
+        spacing='0.8rem'
+      >
+        <For each={subnets}>
+          {({ network, id }) => (
+            <Slider
+              min={min}
+              max={max}
+              value={[
+                network.address.asFullDecimal(),
+                network.broadcast.asFullDecimal(),
+              ]}
+              disabled
+              disableSwap
+              className={cn('[&>.MuiSlider-thumb]:size-[0.6rem]')}
+              key={`slider-for-network-${id}`}
+            />
+          )}
+        </For>
+      </VStack>
+    </ZStack>
   );
 }
