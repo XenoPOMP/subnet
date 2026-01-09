434f68a70b8075cab7a9c61322af2d5e42077cdd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-09 12:52:07 +0200

    🧹 remove deprecated hook

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -2,12 +2,4 @@
 
 import { useClipboard } from 'use-clipboard-copy';
 
-/**
- * This hook allows copy text to clipboard.
- *
- * @deprecated Use **useClipboard** from `'use-clipboard-copy'` instead
- */
-const useCopyToClipboard = useClipboard;
-
-// eslint-disable-next-line deprecation/deprecation
-export default useCopyToClipboard;
+export default useClipboard;
