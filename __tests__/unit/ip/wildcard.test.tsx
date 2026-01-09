bbe6450220d9aa3695ee59d7a4623073c4df914d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-09 13:54:30 +0200

    🧪 optimize code

--- __tests__/unit/ip/wildcard.test.tsx
+++ __tests__/unit/ip/wildcard.test.tsx
@@ -1,19 +1,21 @@
 import { describe, expect, test } from 'vitest';
 
-import { decimal } from '@/utils/base-number';
 import { Address, Network } from '@/utils/ip';
 
 describe('Network.wildcard()', () => {
   test('It works', () => {
     const ip = new Address(192, 168, 0, 0);
-    const net = new Network(ip, 28);
-    const wildcard = decimal(net.wildcard().asFullDecimal()).binary().value;
+    const network = new Network(ip, 28);
+    const wildcard = network.wildcard().format({
+      grade: 'binary',
+      delimiters: false,
+    });
     const nextWildcard = '1'.repeat(
-      decimal(net.wildcard().asFullDecimal()).binary().value.length + 1,
+      wildcard.split('').filter(c => c === '1').length + 1,
     );
 
     // Run checks
-    expect(wildcard).toEqual('1111');
+    expect(wildcard).toEqual('1111'.padStart(32, '0'));
     expect(nextWildcard).toEqual('11111');
   });
 });
