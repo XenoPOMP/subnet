9a327bfe7a36b2a776f9eb8a7d308aa9fe046124
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-09 16:05:41 +0200

    ✍️ save form states globally

--- src/zustand/stores/useNetworkStore.ts
+++ src/zustand/stores/useNetworkStore.ts
@@ -1,11 +1,99 @@
 'use client';
 
+import lodashSet from 'lodash.set';
 import { v4 as uuid } from 'uuid';
+import type { Lenient } from 'xenopomp-essentials';
 import { create } from 'zustand';
 
 import { Address, Network } from '@/utils/ip';
 
-export interface INetworkStore {
+export const useNetworkStore = create<INetworkStore & NetworkFormDelegate>(
+  (set, get) => ({
+    root: null,
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    updateRootNetwork(root) {
+      set({
+        root,
+      });
+    },
+
+    subnets: [],
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    createSubnet() {
+      const id = uuid();
+      const previousSubnets = get().subnets;
+      previousSubnets.push({
+        id,
+        network: get().root ?? new Network(new Address(127, 0, 0, 1), 24),
+      });
+
+      set({
+        subnets: previousSubnets,
+      });
+
+      return id;
+    },
+
+    // FIXME When removing items, networks are reset on other items.
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    removeSubnet(id) {
+      const prevSubnets = get().subnets;
+      const newSubnets = prevSubnets.filter(n => n.id !== id);
+      set({
+        subnets: newSubnets,
+      });
+    },
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    updateSubnet(id, net) {
+      const prevSubnets = get().subnets;
+      const newSubnets = prevSubnets.map(n =>
+        n.id === id
+          ? {
+              id,
+              network: net,
+            }
+          : n,
+      );
+
+      set({
+        subnets: newSubnets,
+      });
+    },
+
+    form: {
+      root: {
+        input: '',
+        error: undefined,
+      },
+    },
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    setValue: (target, newValue) => {
+      set({
+        form: lodashSet(get().form, `${target}.input`, newValue),
+      });
+    },
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    setError: (target, newError) => {
+      set({
+        form: lodashSet(get().form, `${target}.error`, newError),
+      });
+    },
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    clearError: target => {
+      set({
+        form: lodashSet(get().form, `${target}.error`, undefined),
+      });
+    },
+  }),
+);
+
+interface INetworkStore {
   root: Network | null;
   updateRootNetwork: (net: Network) => void;
 
@@ -18,58 +106,23 @@ export interface INetworkStore {
   updateSubnet: (id: string, net: Network) => void;
 }
 
-export const useNetworkStore = create<INetworkStore>((set, get) => ({
-  root: null,
+interface FormInput {
+  input: string;
+  error?: string;
+}
 
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  updateRootNetwork(root) {
-    set({
-      root,
-    });
-  },
+interface NetworkForm {
+  form: {
+    root: FormInput;
+  } & Partial<Record<Lenient<string>, FormInput>>;
+}
 
-  subnets: [],
+type Target = keyof NetworkForm['form'];
 
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  createSubnet() {
-    const id = uuid();
-    const previousSubnets = get().subnets;
-    previousSubnets.push({
-      id,
-      network: get().root ?? new Network(new Address(127, 0, 0, 1), 24),
-    });
+interface NetworkFormMethods {
+  setValue: (target: Target, newValue: string) => void;
+  setError: (target: Target, newError: string | undefined) => void;
+  clearError: (target: Target) => void;
+}
 
-    set({
-      subnets: previousSubnets,
-    });
-
-    return id;
-  },
-
-  // FIXME When removing items, networks are reset on other items.
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  removeSubnet(id) {
-    const prevSubnets = get().subnets;
-    const newSubnets = prevSubnets.filter(n => n.id !== id);
-    set({
-      subnets: newSubnets,
-    });
-  },
-
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  updateSubnet(id, net) {
-    const prevSubnets = get().subnets;
-    const newSubnets = prevSubnets.map(n =>
-      n.id === id
-        ? {
-            id,
-            network: net,
-          }
-        : n,
-    );
-
-    set({
-      subnets: newSubnets,
-    });
-  },
-}));
+type NetworkFormDelegate = NetworkForm & NetworkFormMethods;
