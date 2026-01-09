021affb2ba553f51ddf7a83d35c71cf6f9a7a44a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-09 13:57:30 +0200

    🪜 calculate step with wildcard

--- src/components/ui/NetSlider/NetSlider.tsx
+++ src/components/ui/NetSlider/NetSlider.tsx
@@ -1,7 +1,10 @@
-import { Slider } from '@mui/material';
 import cn from 'classnames';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import type { FC } from 'react';
+import { useMemo } from 'react';
 
+import { binary } from '@/utils/base-number';
 import type { Network } from '@/utils/ip';
 import { useNetworkStore } from '@/zustand';
 
@@ -16,17 +19,39 @@ export const NetSlider: FC<Props> = ({ network }) => {
   const min = root?.address.asFullDecimal();
   const max = root?.broadcast.asFullDecimal();
 
+  const step = useMemo((): number => {
+    const wildcard = network.wildcard().format({
+      grade: 'binary',
+      delimiters: false,
+    });
+    const nextWildcard = '1'.repeat(
+      wildcard.split('').filter(c => c === '1').length + 1,
+    );
+    return binary(nextWildcard)!.decimal().value;
+
+    // .wildcard() method uses .mask property under the hood.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [network, network.mask]);
+
   return (
-    <Slider
-      min={min}
-      max={max}
-      value={[
-        network.address.asFullDecimal(),
-        network.broadcast.asFullDecimal(),
-      ]}
-      disabled
-      disableSwap
-      className={cn('[&>.MuiSlider-thumb]:size-[0.6rem]', 'z-[20]')}
-    />
+    <div>
+      <Slider
+        min={min}
+        max={max}
+        value={[
+          network.address.asFullDecimal(),
+          network.broadcast.asFullDecimal(),
+        ]}
+        // disabled
+        // disableSwap
+        range={{
+          // editable: false,
+          draggableTrack: true,
+        }}
+        className={cn('[&>.MuiSlider-thumb]:size-[0.6rem]', 'z-[20]')}
+      />
+
+      <p>Mask (CIDR): {network.mask}</p>
+    </div>
   );
 };
