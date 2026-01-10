19a748f748b67d4e47cd82acd73268df109cfd73
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-10 15:21:24 +0200

    🖍️ add tooltip

--- src/components/ui/kit/ColorPicker/ColorPicker.tsx
+++ src/components/ui/kit/ColorPicker/ColorPicker.tsx
@@ -1,27 +1,45 @@
 'use client';
 
-import type { Chrome } from '@uiw/react-color';
+import { Colorful } from '@uiw/react-color';
 import cn from 'classnames';
 import randomColor from 'randomcolor';
 import type { ComponentProps, FC } from 'react';
 import { useState } from 'react';
 
+import { WithTooltip } from '@/components/ui/kit';
+import { useUniqueId } from '@/hooks';
+
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const ColorPicker: FC<Props> = ({ color: stateColor, onChange }) => {
   const [color, setColor] = useState<string>(stateColor ?? randomColor());
+  const uuid = useUniqueId(g => `color-picker-${g}`);
 
   return (
-    <div>
+    <WithTooltip
+      id={uuid}
+      tooltip={
+        <>
+          <Colorful
+            color={color}
+            onChange={col => {
+              setColor(col.hex);
+              onChange?.(col);
+            }}
+          />
+        </>
+      }
+      place='right'
+    >
       <div
-        className={cn('size-[1.6rem] cursor-pointer rounded-full')}
+        className={cn('size-[1.6rem] rounded-full')}
         style={{
           backgroundColor: color,
         }}
       ></div>
-    </div>
+    </WithTooltip>
   );
 };
 
-type Props = Pick<ComponentProps<typeof Chrome>, 'onChange'> & {
+type Props = Pick<ComponentProps<typeof Colorful>, 'onChange'> & {
   color?: string;
 };
