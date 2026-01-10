f3fad66920c1f341dca0d1915b621cd9e60b8d47
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-10 20:40:57 +0200

    🚫 remove serwist support

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,5 @@ const baseConfig: NextConfig = {
 
 export default config(baseConfig, {
   mdx: true,
-  serwist: {
-    cacheOnNavigation: true,
-    swSrc: 'app/sw.ts',
-    swDest: 'public/sw.js',
-  },
+  serwist: false,
 });
