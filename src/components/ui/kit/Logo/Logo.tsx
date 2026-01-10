a2f650c5ba866acaf9bc4770fdd87ee250986565
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-10 11:35:10 +0200

    🌅 create first style of logo

--- src/components/ui/kit/Logo/Logo.tsx
+++ src/components/ui/kit/Logo/Logo.tsx
@@ -3,24 +3,39 @@ import Link from 'next/link';
 import type { VariableFC } from 'xenopomp-essentials';
 
 import type { LogoProps } from './Logo.props';
+import type { LogoVariants } from './Logo.variants';
+import { logoVariantsVariants } from './Logo.variants';
+import LogoDefault from './icons/default';
 
 /**
  * App`s logo component. Contains link to root
  * page (can be overwritten).
  * @constructor
  */
-export const Logo: VariableFC<typeof Link, LogoProps, 'children' | 'href'> = ({
-  href = '/',
-  className,
-  ...props
-}) => {
+export const Logo: VariableFC<
+  typeof Link,
+  LogoProps & LogoVariants,
+  'children' | 'href'
+> = ({ href = '/', className, variant, painting, ...props }) => {
   return (
     <Link
       href={href}
-      className={cn('lowercase', className)}
+      className={cn(className)}
       {...props}
     >
-      xenopomp
+      {variant === 'long' || !variant ? (
+        <LogoDefault
+          className={cn(
+            logoVariantsVariants({ variant, painting }),
+            'h-[20px]',
+          )}
+          style={{
+            aspectRatio: 133 / 24,
+          }}
+        />
+      ) : (
+        <></>
+      )}
     </Link>
   );
 };
