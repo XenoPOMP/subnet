17b4bcfe4360ce01cd1c5ac7fb89a21dd31fb0ef
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-10 11:43:59 +0200

    🧑‍💻 add logo to header

--- src/components/ui/kit/TitleBar/TitleBar.tsx
+++ src/components/ui/kit/TitleBar/TitleBar.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames';
 import type { VariableFC } from 'xenopomp-essentials';
 
 import { HStack, Spacer } from '@/components/ui';
+import { Logo } from '@/components/ui/kit';
 
 // TODO Add logo here
 // TODO Add traffic lights here
@@ -19,14 +20,13 @@ export const TitleBar: VariableFC<'header', unknown, 'children'> = ({
         className={cn(
           'h-[--dashboard-header-height]',
           'p-[1.6rem]',
-          // eslint-disable-next-line prettier/prettier
           'border-b-[1px] border-b-divider',
           'select-none',
           className,
         )}
         {...props}
       >
-        <p>Traffic lights</p> <Spacer /> <p>Header</p>
+        <p>Traffic lights</p> <Spacer /> <Logo />
       </header>
     </HStack>
   );
