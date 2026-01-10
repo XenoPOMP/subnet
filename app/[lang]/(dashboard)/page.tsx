d8ded3947040c5ad9507a4762284cd8602a17f2f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-01-10 20:20:13 +0200

    ✍️ add comment

--- app/[lang]/(dashboard)/page.tsx
+++ app/[lang]/(dashboard)/page.tsx
@@ -9,6 +9,7 @@ import { useTranslations } from '@/i18n';
 
 import { NetForm } from './components';
 
+// TODO Add adaptive layout later (maybe). 870x650 is minimal sizes for now.
 // eslint-disable-next-line jsdoc/require-jsdoc
 export default function HomePage() {
   const { t } = useTranslations();
