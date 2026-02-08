97f03a4ffb28267dab3f07583ce5ac548bc6027d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-02-08 16:47:24 +0200

    ✍️ change ui

--- app/[lang]/share/components/ShareMapButton/ShareMapButton.tsx
+++ app/[lang]/share/components/ShareMapButton/ShareMapButton.tsx
@@ -1,6 +1,14 @@
+import { Link } from 'lucide-react';
 import type { FC } from 'react';
+
+import { Button } from '@/components/ui/kit';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const ShareMapButton: FC<unknown> = () => {
-  return <div></div>;
+  return (
+    <Button
+      square
+      leadingIcon={Link}
+    />
+  );
 };
