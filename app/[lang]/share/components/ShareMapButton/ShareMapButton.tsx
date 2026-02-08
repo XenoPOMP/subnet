f14d6a114fcf0efd80fc8f8cc06061f5453f46ff
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-02-08 17:48:51 +0200

    ✍️ change impl

--- app/[lang]/share/components/ShareMapButton/ShareMapButton.tsx
+++ app/[lang]/share/components/ShareMapButton/ShareMapButton.tsx
@@ -1,14 +1,22 @@
+'use client';
+
 import { Link } from 'lucide-react';
 import type { FC } from 'react';
 
-import { Button } from '@/components/ui/kit';
+import { CopyTextButton } from '@/components/ui/kit';
+import { useTranslations } from '@/i18n';
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const ShareMapButton: FC<unknown> = () => {
+  const { t } = useTranslations();
+
+  // TODO Generate proper link
   return (
-    <Button
+    <CopyTextButton
       square
       leadingIcon={Link}
+      text='copy link'
+      textOnCopy={t.copyTextMessages.shareNetwork.shared}
     />
   );
 };
