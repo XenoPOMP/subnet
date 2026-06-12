d77e0403e4648ae70cd96edb4ee74e7f304d60b3
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-06-12 16:52:17 +0200

    🧑‍💻 refactor code

--- __tests__/unit/base-number/forbidden-apis.test.tsx
+++ __tests__/unit/base-number/forbidden-apis.test.tsx
@@ -4,10 +4,27 @@ import { PrivateApiError } from '@/errors';
 import { BaseNumber } from '@/utils/base-number/impl';
 
 describe('BaseNumber class is not meant to be used standalone', () => {
-  test('Conversions throws', () => {
+  describe('Conversions throws', () => {
     const num = new BaseNumber(12, 'decimal');
-
-    expect(() => num.decimal()).toThrow(PrivateApiError);
-    expect(() => num.binary()).toThrow(PrivateApiError);
+    testMethod(num, 'decimal');
+    testMethod(num, 'binary');
   });
 });
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function testMethod(number: BaseNumber<any, any>, method: PrivateMethods) {
+  test(`BaseNumber.${method}() is forbidden`, () =>
+    expect(() => number.decimal()).toThrow(PrivateApiError));
+}
+
+type PrivateMethods = keyof Omit<
+  {
+    [Key in keyof BaseNumber<any, any> as BaseNumber<
+      any,
+      any
+    >[Key] extends () => any
+      ? Key
+      : never]: string;
+  },
+  'value'
+>;
