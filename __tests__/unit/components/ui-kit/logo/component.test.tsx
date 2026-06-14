89c06dbd37d8dcdf7b40b9bd7073d0b54ec0a58c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2026-06-14 20:25:20 +0200

    🧪 fix dependency

--- __tests__/unit/components/ui-kit/logo/component.test.tsx
+++ __tests__/unit/components/ui-kit/logo/component.test.tsx
@@ -1,10 +1,35 @@
-import { describe, test } from 'vitest';
-import { assertRendering } from 'xenopomp-essentials/vitest';
+import type { usePathname, useRouter } from 'next/navigation';
+import { describe, test, vi } from 'vitest';
+import { assertRendering, injectMocks } from 'xenopomp-essentials/vitest';
 
 import { Logo } from '@/components/ui/kit';
 
 describe('Logo component', () => {
+  injectMocks(() => {
+    vi.mock('next/navigation', () => {
+      return {
+        /** Mock implementation for useRouter */
+        useRouter: (): Partial<ReturnType<typeof useRouter>> => {
+          return {
+            /** Mock implementation for useRouter().push */
+            push() {},
+          };
+        },
+
+        // eslint-disable-next-line jsdoc/require-jsdoc
+        useSearchParams(): URLSearchParams {
+          return new URLSearchParams();
+        },
+
+        /** Mock implementation for usePathname */
+        usePathname: (): Partial<ReturnType<typeof usePathname>> => {
+          return '/';
+        },
+      };
+    });
+  });
+
   test('It renders', () => {
-    assertRendering(<Logo />);
+    assertRendering(<Logo href='/' />);
   });
 });
